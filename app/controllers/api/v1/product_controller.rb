4f28fbc0ba1f733947b11e3255b822c9cc422d18
Jurupoc <jurupoc222@gmail.com>
2023-05-21 23:11:04 -0300

    Creating Initial Routes.

---
+++ app/controllers/api/v1/product_controller.rb
@@ -0,0 +1,20 @@
+module Api
+	module V1
+		class ProductsController < Api::V1::ApiController
+			def index
+			end
+
+			def create
+			end
+
+			def update
+			end
+			
+			def get
+			end
+
+			def delete
+			end
+		end
+	end
+end
