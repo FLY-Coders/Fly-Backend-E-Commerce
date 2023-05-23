ed28f75e0e427568a9a4928f4062566d66fd7bdd
Jurupoc <jurupoc222@gmail.com>
2023-05-22 21:12:07 -0300

    Removing Unnecessary Comments.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,19 +2,19 @@ module Api
 	module V1
 		class ProductsController < ApplicationController
 
-			def index  # GET ALL
+			def index
 				products = Product.order('created_at DESC')
 				render json: {status: 'SUCCESS', message: 'Loaded Products', data: products}, status: :ok
 				return
 			end
 
-			def show  # GET BY ID
+			def show
 				product = Product.find(params[:id])
 				render json: {status: 'SUCCESS', message: 'Loaded Product', data: product}, status: :ok
 				return
 			end
 
-			def create  # CREATE
+			def create
 				product = Product.new(product_params)
 				if product.save
 					render json: {status: 'SUCCESS', message: 'Saved Product', data: product}, status: :created
